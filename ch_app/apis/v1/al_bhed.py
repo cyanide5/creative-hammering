c12f95504fb2040c9b2fd8ffa091c47e0136aee7
namex <namexremoved@gmil.com>
2021-10-18 20:29:36 -0500

    added input dropdown and input field for albhed endpoint

--- ch_app/apis/v1/al_bhed.py
+++ ch_app/apis/v1/al_bhed.py
@@ -7,23 +7,22 @@ from ch_app.bll import al_bhed
 
 api = Namespace("v1/al_bhed", description="Translate English to AlBhed")
 
+parser = reqparse.RequestParser()
+parser.add_argument('input_type', type=str, required=True, choices=(["eng", "alb"]))
+parser.add_argument('input_string', type=str, required=True)
+
 logger = logging.getLogger(__name__)
 
-@api.route("")
+@api.route('/translate')
 @api.response(200, "you did it!")
 @api.response(400, "you fucked it up")
 class AlBhed(Resource):
-    phrase = api.model("Phrase",
-    {"phrase": fields.String(required=True, example="This is a phrase")}
-    )
-    def get(self):
-        logger.info("Initializing Translation")
-        try:
-            body = json.loads(request.data.decode("utf-8", errors="ignore"))
-        except json.JSONDecodeError:
-            logger.error("JSON decoder error while parsing the payload")
 
-        if body['phrase']:
-            return al_bhed.translate(body['phrase'])
+    @api.expect(parser)
+    def get(self):
+        args = parser.parse_args()
+        logger.info("Initializing Translation")
+
+        return
 
 
